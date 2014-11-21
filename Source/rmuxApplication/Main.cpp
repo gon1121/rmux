4cba1db9daa2adb63a00cf8ab81a74637d614852
Tommy Gon Lee <gon1121@gmail.com>
2014-11-21 16:07:30 +0900

    add flash file

--- Source/rmuxApplication/Main.cpp
+++ Source/rmuxApplication/Main.cpp
@@ -11,14 +11,27 @@
 
 #include <Vision/Runtime/Framework/VisionApp/Modules/VHelp.hpp>
 
+#include <Vision/Runtime/EnginePlugins/ThirdParty/ScaleformEnginePlugin/VScaleformMovie.hpp>
+#include <Vision/Runtime/EnginePlugins/ThirdParty/ScaleformEnginePlugin/VScaleformVariable.hpp>
+#include <Vision/Runtime/EnginePlugins/ThirdParty/ScaleformEnginePlugin/VScaleformValue.hpp>
+#include <Vision/Runtime/EnginePlugins/VisionEnginePlugin/Input/VFreeCamera.hpp>
+
+//#include "GFx/GFx_Player.h"
+
 // Use the following line to initialize a plugin that is statically linked.
 // Note that only Windows platform links plugins dynamically (on Windows you can comment out this line).
 VIMPORT IVisPlugin_cl* GetEnginePlugin_rmuxPlugin();
+VIMPORT IVisPlugin_cl* GetEnginePlugin_vScaleformPlugin();
 
-class rmuxApplicationClass : public VAppImpl
+class rmuxApplicationClass : public VAppImpl, public IVisCallbackHandler_cl
 {
 public:
-  rmuxApplicationClass() {}
+  rmuxApplicationClass() 
+	  : m_sMovieName("")
+	  , m_spMovie(NULL)
+	  , m_spMouse(NULL)
+  {}
+
   virtual ~rmuxApplicationClass() {}
 
   virtual void SetupAppConfig(VisAppConfig_cl& config) HKV_OVERRIDE;
@@ -28,6 +41,14 @@ public:
   virtual void AfterSceneLoaded(bool bLoadingSuccessful) HKV_OVERRIDE;
   virtual bool Run() HKV_OVERRIDE;
   virtual void DeInit() HKV_OVERRIDE;
+
+  void OnHandleCallback(IVisCallbackDataObject_cl *pData) HKV_OVERRIDE;
+
+private:
+	VString m_sMovieName;
+	VScaleformMovieInstancePtr m_spMovie;
+	VScaleformMovieInstancePtr m_spCounter;
+	VisScreenMaskPtr m_spMouse;
 };
 
 VAPP_IMPLEMENT_SAMPLE(rmuxApplicationClass);
@@ -39,10 +60,10 @@ void rmuxApplicationClass::SetupAppConfig(VisAppConfig_cl& config)
 
   // Set the initial starting position of our game window and other properties
   // if not in fullscreen. This is only relevant on windows
-  config.m_videoConfig.m_iXRes = 1280; // Set the Window size X if not in fullscreen.
-  config.m_videoConfig.m_iYRes = 720;  // Set the Window size Y if not in fullscreen.
-  config.m_videoConfig.m_iXPos = 50;   // Set the Window position X if not in fullscreen.
-  config.m_videoConfig.m_iYPos = 50;   // Set the Window position Y if not in fullscreen.
+  config.m_videoConfig.m_iXRes = 640; // Set the Window size X if not in fullscreen.
+  config.m_videoConfig.m_iYRes = 1136;  // Set the Window size Y if not in fullscreen.
+  config.m_videoConfig.m_iXPos = -660;   // Set the Window position X if not in fullscreen.
+  config.m_videoConfig.m_iYPos = -30;   // Set the Window position Y if not in fullscreen.
 
   // Name to be displayed in the windows title bar.
   config.m_videoConfig.m_szWindowTitle = "rmux";
@@ -69,6 +90,7 @@ void rmuxApplicationClass::PreloadPlugins()
   // you still need to statically link your plugin library (e.g. on mobile platforms) through project
   // Properties, Linker, Additional Dependencies.
   VISION_PLUGIN_ENSURE_LOADED(rmuxPlugin);
+  VISION_PLUGIN_ENSURE_LOADED(vScaleformPlugin);
 }
 
 //---------------------------------------------------------------------------------------------------------
@@ -81,6 +103,8 @@ void rmuxApplicationClass::Init()
   VisAppLoadSettings settings("Scenes/Default.vscene");
   settings.m_customSearchPaths.Append(":template_root/Assets");
   LoadScene(settings);
+
+  VOnExternalInterfaceCall::OnExternalInterfaceCallback += this;
 }
 
 //---------------------------------------------------------------------------------------------------------
@@ -94,11 +118,29 @@ void rmuxApplicationClass::AfterSceneLoaded(bool bLoadingSuccessful)
   help.Append("");
   RegisterAppModule(new VHelp(help));
 
-  // Create a mouse controlled camera (set above the ground so that we can see the ground)
-  Vision::Game.CreateEntity("VisMouseCamera_cl", hkvVec3(0.0f, 0.0f, 170.0f));
+  // Create a free camera
+  VFreeCamera* pFreeCamera = vstatic_cast<VFreeCamera*>(Vision::Game.CreateEntity("VFreeCamera", hkvVec3::ZeroVector()));
+  pFreeCamera->SetThinkFunctionStatus(false);
 
-  // Add other initial game code here
-  // [...]
+  // Create the mouse cursor
+  m_spMouse = new VisScreenMask_cl("Textures\\UI\\mouseNorm.TGA");
+  m_spMouse->SetTransparency(VIS_TRANSP_ALPHA);
+  m_spMouse->SetVisible(true);
+  VScaleformManager::GlobalManager().SetHandleCursorInput(true);
+
+  VString strScoreName = "Flash\\ingame.swf";
+  m_spCounter = VScaleformManager::GlobalManager().LoadMovie(strScoreName);
+  if (m_spCounter==NULL)
+  {
+	  hkvLog::FatalError("Could not load movie: %s", strScoreName);
+  }
+  else
+  {
+	  VScaleformVariable myScore = m_spCounter->GetVariable("_root.myScore");
+	  VScaleformValue value[1];
+	  value[0] = 5000;
+	  myScore.Invoke("add", value);
+  }
 }
 
 //---------------------------------------------------------------------------------------------------------
@@ -106,13 +148,71 @@ void rmuxApplicationClass::AfterSceneLoaded(bool bLoadingSuccessful)
 //---------------------------------------------------------------------------------------------------------
 bool rmuxApplicationClass::Run()
 {
-  return true;
+	float x,y;
+	VScaleformManager::GlobalManager().GetCursorPos(x,y);
+	m_spMouse->SetPos(x,y);
+	
+	return true;
 }
 
 void rmuxApplicationClass::DeInit()
 {
-  // De-Initialization
-  // [...]
+	// De-Initialization
+	// [...]
+	VOnExternalInterfaceCall::OnExternalInterfaceCallback -= this;
+}
+
+//---------------------------------------------------------------------------------------------------------
+// Receive command callbacks and external interface calls.
+//---------------------------------------------------------------------------------------------------------
+void rmuxApplicationClass::OnHandleCallback(IVisCallbackDataObject_cl *pData)
+{
+	if(pData->m_pSender == &VOnExternalInterfaceCall::OnExternalInterfaceCallback)
+	{  
+		VOnExternalInterfaceCall *pExternalCall = (VOnExternalInterfaceCall *)pData;
+
+		if(pExternalCall->m_sMethodName == "resetButton")
+		{
+			Vision::Message.Add(1, "Reset button pressed");
+
+			VScaleformVariable counterVar = m_spMovie->GetVariable("_root.globalCount");
+
+#if 1
+			counterVar.SetNumber(0);
+			VScaleformVariable labelVar =  m_spMovie->GetVariable("_root.countLabel.labelText.text");
+			labelVar.SetString(":)");
+
+#else
+			VScaleformValue counter = m_spMovie->GetVariableValue("_root.globalCount");
+			VScaleformValue label =  m_spMovie->GetVariableValue("_root.countLabel.labelText.text");
+			counter.SetNumber(0);
+			label.SetString("-");
+#endif
+
+			return;
+		}
+		if(pExternalCall->m_sMethodName == "addButton")
+		{
+			Vision::Message.Add(1, "Add 1 button pressed");
+
+			//VASSERT(m_spMovie->GetVariable("_root.globalCount")->IsNumeric()); // var globalCount:Number;
+			int countValue = static_cast<int>(m_spMovie->GetVariable("_root.globalCount").GetNumber());
+
+			if (countValue == 1)
+			{
+				VScaleformVariable titleVar =  m_spMovie->GetVariable("_root.title.text");
+				titleVar.SetString("Count to 10 to Exit!");
+			}
+			else if (countValue == 10)
+			{
+				VScaleformVariable labelVar =  m_spMovie->GetVariable("_root.countLabel.labelText.text");
+				labelVar.SetString("Bye!");
+				VAppBase::Get()->Quit();
+			}
+
+			return;
+		}
+	}
 }
 
 /*
